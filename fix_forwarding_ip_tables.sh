9d4ea2d9801da336ba1198c109e33fa5b7865a5a
Irvin H <irvinhomem@gmail.com>
2016-06-17 16:35:30 +0200

    Backing up

--- fix_forwarding_ip_tables.sh
+++ fix_forwarding_ip_tables.sh
@@ -8,4 +8,6 @@ echo 1 > /proc/sys/net/ipv4/ip_forward
 #Check that the ip_forward flag has been set
 cat /proc/sys/net/ipv4/ip_forward
 
-#iptables -t nat -A POSTROUTING -o 
+#Fix iptables rules
+cat /proc/net/dev
+#iptables -t nat -A POSTROUTING -o
