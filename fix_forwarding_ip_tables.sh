97a6b561f6f4ee9651897f2b90304d160786dfe9
Irvin H <irvinhomem@gmail.com>
2016-06-17 16:15:25 +0200

    Added server side ip forwarding fixes

---
+++ fix_forwarding_ip_tables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "This is for the SERVER side !"
+
+#Fix IP forwarding
+echo 1 > /proc/sys/net/ipv4/ip_forward
+
+#Check that the ip_forward flag has been set
+cat /proc/sys/net/ipv4/ip_forward
