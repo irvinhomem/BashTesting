f77628b9efc02cee883b1e48b756d95d5eafec7e
Irvin H <irvinhomem@gmail.com>
2016-06-18 18:01:43 +0200

    Working ip forward adjustment and iptables rules

--- fix_forwarding_ip_tables.sh
+++ fix_forwarding_ip_tables.sh
@@ -1,17 +1,27 @@
 #!/bin/bash
 
+# Make sure that config.sh is "executable" by running
+#chmod u+x ./config.sh
+# Get/initialize/run declared variables from config.sh
+source config.sh
+#. ./config.sh
+
 echo "This is for the SERVER side !"
 
-#Fix IP forwarding
+# Fix IP forwarding
 echo 1 > /proc/sys/net/ipv4/ip_forward
 
-#Check that the ip_forward flag has been set
+# Check that the ip_forward flag has been set
 cat /proc/sys/net/ipv4/ip_forward
 
-#Fix iptables rules
-#Get the name of the ethernet interface
+# Fix iptables rules
+# Get the name of the ethernet interface
 #cat /proc/net/dev
 
-iptables -t nat -A POSTROUTING -o $TUN_ETH_NIC -j MASQUERADE
-iptables -A FORWARD -i $TUN_ETH_NIC -o dns0 -m state --state RELATED,ESTABLISHED -j ACCEPT
-iptables -A FORWARD -i dns0 -o $TUN_ETH_NIC -j ACCEPT
+# Check the name of the interface:
+echo "Ethernet Interface: " ${eth_if}
+
+# Run iptables rules
+iptables -t nat -A POSTROUTING -o ${eth_if} -j MASQUERADE
+iptables -A FORWARD -i ${eth_if} -o dns0 -m state --state RELATED,ESTABLISHED -j ACCEPT
+iptables -A FORWARD -i dns0 -o ${eth_if} -j ACCEPT
