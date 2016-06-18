046723d6f38be87a1cb45a85a2eb861eb238b77c
Irvin H <irvinhomem@gmail.com>
2016-06-18 18:32:22 +0200

    Backing up

--- run_tunnel_client.sh
+++ run_tunnel_client.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
 
-source config.sh
+# Make sure that config.sh is executable
+#chmod u+x config.sh
 
-echo "Tun Server: " $TUN_SERVER
+# Get variables from config.sh
+source config.sh
+echo "--"
+echo "### Hopefully this is running on the CLIENT side ! ###"
+echo "Tun Server: " ${tun_server}
+
+# Run the Tunnel client commands
+
+#Fix the routing
+# 1. Add a route to the DNS server through the normal ethernet default gateway
+
+# 2. Remove the current default gateway (to make sure that all traffic goes through the tunnel interface)
+
+# 3. Add the tunnel interface as the default gateway
+
+# Ping google.com to see if there is communication happening through the tunnel (watch the response on the server side)
