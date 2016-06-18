4fe042712f2626de45bb21b40067534466d036b0
Irvin H <irvinhomem@gmail.com>
2016-06-18 18:33:25 +0200

    Adding tunnel server script

---
+++ run_tunnel_server.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Make sure that config.sh is executable
+#chmod u+x config.sh
+
+# Get variables from config.sh
+source config.sh
+echo "--"
+echo "### Hopefully this is running on the SERVER side ! ###"
