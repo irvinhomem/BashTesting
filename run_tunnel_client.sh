b5b4e37ae56510262788a5195c3d7bed4b01079c
Irvin H <irvinhomem@gmail.com>
2016-06-20 20:31:26 +0200

    Added some delays to ensure prior commands finish

--- run_tunnel_client.sh
+++ run_tunnel_client.sh
@@ -27,9 +27,12 @@ for net_if in $(ls -1 /sys/class/net) ;do
     route add -host ${local_dns} gw ${def_gw}
     # 2. Remove the current default gateway (to make sure that all traffic goes through the tunnel interface)
     #   Needs to run as sudo
+    sleep 5
     route del default
     # 3. Add the tunnel interface as the default gateway
     #   Needs to run as sudo
+
+    sleep 5
     route add default dev ${dns_tun_if} gw ${inside_tun_server}
 
     # TEST
